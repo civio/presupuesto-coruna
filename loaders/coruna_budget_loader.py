3d1eaa3762c2e05600e1047d1da8bbcb72d0a4eb
Eduardo Sebastian <esebastian@altiora.es>
2017-01-16 19:36:06 +0100

    Improve the cleaning up of entry descriptions

--- loaders/coruna_budget_loader.py
+++ loaders/coruna_budget_loader.py
@@ -55,7 +55,7 @@ class CorunaBudgetLoader(SimpleBudgetLoader):
                 'ec_code': ec_code[:-2],        # First three digits (everything but last two)
                 'ic_code': line[2].rjust(4, '0') + ("-B" if int(year) >= 2016 else "-A"),  # We add prefixing zeroes and manage changes after 2015
                 'item_number': ec_code[-2:],    # Last two digits
-                'description': line[5].capitalize().rstrip('.'),
+                'description': self._spanish_titlecase(line[5]).rstrip('.'),
                 'amount': self._parse_amount(line[9 if is_actual else 6])
             }
 
@@ -68,6 +68,6 @@ class CorunaBudgetLoader(SimpleBudgetLoader):
                 'ec_code': ec_code[:3],          # First three digits
                 'ic_code': '0000',               # All income goes to the root node
                 'item_number': ec_code[-2:],     # Last two digits
-                'description': line[3].capitalize().rstrip('.'),
+                'description': self._spanish_titlecase(line[3]).rstrip('.'),
                 'amount': self._parse_amount(line[7 if is_actual else 4])
             }
