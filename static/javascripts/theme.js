41586b94008a462c9179ebb383f9447051f66970
Eduardo Sebastian <esebastian@altiora.es>
2017-02-09 20:34:26 +0100

    WIP add formater function

--- static/javascripts/theme.js
+++ static/javascripts/theme.js
@@ -1,6 +1,7 @@
 // Theme custom js methods
 $(document).ready(function(){
 
+  // Custom warnings and notes
   var addChartsAlert = function(selector) {
     var str = {
       'es': 'Los datos de ejecución del año 2016 son provisionales y están pendientes del cierre de ejercicio',
@@ -31,4 +32,67 @@ $(document).ready(function(){
   addChartsAlert('.sankey-container');
 
   addBudgetClarification();
+
 });
+
+// Custom data table formatters
+function getFormatter(formatter, stats, year, getter) {
+  // theme.js version
+  // Pretty print a number by inserting ',' thousand separator
+  function nominalFormatter(value, type, item) {
+    if (type === 'filter') return value;  // We filter based on the raw data
+    return formatAmount(value);
+  }
+
+  // Display amount adjusted for inflation (real, versus nominal)
+  function realFormatter(value, type, item) {
+    if (value == null) return '';
+    if (type === 'filter') return value;  // We filter based on the raw data
+    var realValue = adjustInflation(value, stats, year);
+    return formatAmount(realValue);
+  }
+
+  // Display amount as percentage of total
+  function percentageFormatter(value, type, item) {
+    if (value == null) return '';
+    if (type === 'filter') return value;  // We filter based on the raw data
+    if (item.root == null)  // No root => independent object
+      return formatPercentage(1);
+    else
+      return formatPercentage(value / columnValueExtractor(item.root, getter));
+  }
+
+  // Display amount as expense per capita
+  function perCapitaFormatter(value, type, item) {
+    if (value == null) return '';
+    if (type === 'filter') return value;  // We filter based on the raw data
+    // Note value is in cents originally
+    var realValue = adjustInflation(value/100, stats, year);
+
+    // Our stats for year X indicate the population for December 31st of that year so,
+    // since we're adjusting inflation for January 1st it seems more accurate to use the
+    // population for that date, i.e. the one for the last day of the previous year.
+    // XXX: Don't think this is still so, and it's confusing, so I've changed it. But
+    // would like to recheck it.
+    var population = getPopulationFigure(stats, year, item.key);
+
+    return addCurrencySymbol(formatDecimal(realValue / population));
+  }
+
+  // Display execution amount as percentage of total
+  function executionPercentageFormatter(value, type, item) {
+    if (value == null) return '';
+    if (type === 'filter') return value;  // We filter based on the raw data
+    if (item.root == null)  return formatPercentage(1); // No root => independent object
+    
+    return formatPercentage(value / columnValueExtractor(item.root, getter));
+  }
+
+  switch (formatter) {
+    case "nominal":              return nominalFormatter;
+    case "real":                 return realFormatter;
+    case "percentage":           return percentageFormatter;
+    case "per_capita":           return perCapitaFormatter;
+    case "execution_percentage": return executionPercentageFormatter;
+  }
+}
