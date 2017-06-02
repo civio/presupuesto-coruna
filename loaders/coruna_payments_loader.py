08d50d9986d8081b5a405d8d650b5cd57713227f
David Cabo <david.cabo@gmail.com>
2017-06-02 02:33:00 +0200

    Add anonymised payment data civio/presupuesto-management#480

--- loaders/coruna_payments_loader.py
+++ loaders/coruna_payments_loader.py
@@ -8,19 +8,28 @@ class CorunaPaymentsLoader(PaymentsLoader):
 
     # Parse an input line into fields
     def parse_item(self, budget, line):
-        policy = self._spanish_titlecase( re.sub('\.$', '', line[6].strip()) )
         amount = self._read_english_number( line[2].strip() )
         payee = self._titlecase( line[4].strip() )
+        anonymized = (payee == '[Anonimizado]*')
+
+        area = line[6].strip()
+        if area != '[ANTICIPO DE CAJA]':
+            match = re.search('^(\d+)', area)
+            # We got 3- or 4- digit functional codes as input, so add a trailing zero
+            area_id = match.group(1).ljust(4, '0')
+            area = Budget.objects.get_all_descriptions(budget.entity)['functional'][area_id]
+        else:
+            area = '(Anticipo de caja)'
 
         return {
-            'area': policy,
+            'area': area,
             'programme': None,
             'fc_code': None,
             'ec_code': None,
             'date': line[1].strip(),
             'contract_type': None,
             'payee': payee,
-            'anonymized': False,
+            'anonymized': anonymized,
             'description': line[5].strip(),
             'amount': amount
         }
